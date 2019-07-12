f65923ef96d9c61a83d7682e642735fa647409b9
Katherine Lewis <kale8432@colorado.edu>
2019-07-12 07:27:15 -0600

    building js

---
+++ main.js
@@ -0,0 +1,11 @@
+var posInput = $('#positive').val()
+var negInput = $('#negative').val()
+var container = $('.breathContainer')
+var thirty =
+  var sixty =
+    var ninty =
+
+      function breath() {
+
+        container.append(``)
+      }
