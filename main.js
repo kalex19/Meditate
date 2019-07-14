729daeb0e2237f9dbf9feb49369ce6d7f947f828
Katherine Lewis <kale8432@colorado.edu>
2019-07-14 12:20:33 -0600

    pseudo code js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //get from storage
 //append to dom
 //use keyframe for animation
-//timer for key frame based on radio button
+//timer for key frame based on radio button - if 30 seconds then show/expand for 30 seconds before contracting
 //get timer from local storage set in other file
 //adjust animation based on timer
